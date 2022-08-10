1e70ec9930113561c50e05709b57da3ed919c795
Eric <lderic0415@gmail.com>
2022-08-10 09:55:30 -0700

    Change MixinCreativeInventoryScreen injection for forge.

--- src/main/java/fi/dy/masa/malilib/mixin/MixinCreativeInventoryScreen.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinCreativeInventoryScreen.java
@@ -1,7 +1,9 @@
 package fi.dy.masa.malilib.mixin;
 
 
+import net.minecraft.item.ItemGroup;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,10 +15,22 @@ import fi.dy.masa.malilib.event.RenderEventHandler;
 @Mixin(CreativeInventoryScreen.class)
 public abstract class MixinCreativeInventoryScreen
 {
-    @Inject(method = "renderTooltip", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
-            target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;Ljava/util/Optional;II)V"))
+    @Shadow private static int selectedTab;
+    private boolean isSearchTab;
+
+    @Inject(method = "renderTooltip", at = @At("HEAD"))
+    private void onRenderToolTipStart(MatrixStack matrixStack, ItemStack stack, int x, int y, CallbackInfo ci) {
+        isSearchTab = (selectedTab == ItemGroup.SEARCH.getIndex());
+    }
+    // @Inject(method = "renderTooltip", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
+    //         target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;Ljava/util/Optional;II)V"))
+    // Can't use original injection with forge
+    @Inject(method = "renderTooltip", at = @At("RETURN"))
     private void onRenderTooltip(MatrixStack matrixStack, ItemStack stack, int x, int y, CallbackInfo ci)
     {
-        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderTooltipLast(matrixStack, stack, x, y);
+        if (isSearchTab)
+        {
+            ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderTooltipLast(matrixStack, stack, x, y);
+        }
     }
 }
